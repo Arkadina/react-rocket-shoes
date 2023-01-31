f91f1fdb44c10faec69216259236d76fe88d94c3
Arkadina <61208486+Arkadina@users.noreply.github.com>
2023-01-30 21:22:52 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,25 @@ import tenis2g from "./assets/img/tenis-2-galeria.png";
 import tenis3g from "./assets/img/tenis-3-galeria.png";
 import video from "./assets/img/img-video.png";
 import arrow from "./assets/img/icon-arrow.svg";
+import bag from "./assets/img/icon-bag.svg";
 
 function App() {
     return (
-        <div className="container flex w-auto">
-            <header>
+        <div className=" flex flex-col min-h-screen ">
+            <header className="flex absolute justify-between w-full">
                 <img src={logo} />
                 <nav>
                     <ul>
-                        <li>
+                        <li className="inline-block ">
                             <a href="#">HOMEM</a>
                         </li>
-                        <li>
+                        <li className="inline-block">
                             <a href="#">MULHER</a>
                         </li>
-                        <li>
+                        <li className="inline-block">
                             <a href="#">CRIANÇA</a>
                         </li>
-                        <li>
+                        <li className="inline-block">
                             <a href="#">CUSTOMIZAR</a>
                         </li>
                     </ul>
@@ -34,19 +35,21 @@ function App() {
                     <img src="./assets/img/icon-search.svg" alt="" />
                     <input type="search" placeholder="Pesquisar" />
                 </div>
-                <img src="./assets/img/icon-bag.svg" alt="" />
+                <img src={bag} alt="" />
             </header>
-            <main>
-                <div class="left-container">
-                    <h1>Tênis Esportivo Para Corrida Top</h1>
+            <main class="flex flex-row">
+                <div class="bg-background w-1.5">
+                    <h1 className="text-6xl font-bold">
+                        Tênis Esportivo Para Corrida Top
+                    </h1>
                     <p>
                         Esse tênis tem o intuito de te deixar ainda mais
                         fitness, focado, e musculoso.
                     </p>
                     <span>R$ 1.000,00</span>
-                    <button>Comprar</button>
+                    <button className="bg-button text-white">Comprar</button>
                 </div>
-                <div class="right-container">
+                <div class="bg-mainBackground">
                     <div class="right-container-top">
                         <img src={tenis1} alt="" />
                     </div>
@@ -58,26 +61,26 @@ function App() {
                     </div>
                 </div>
             </main>
-            <footer>
+            <footer className="flex flex-row justify-between">
                 <img src={video} alt="" />
 
                 <nav>
                     <ul>
-                        <li>
+                        <li className="inline-block">
                             <a href="#">FACEBOOK</a>
                         </li>
-                        <li>
+                        <li className="inline-block">
                             <a href="#">INSTAGRAM</a>
                         </li>
-                        <li>
+                        <li className="inline-block">
                             <a href="#">YOUTUBE</a>
                         </li>
-                        <li>
+                        <li className="inline-block">
                             <a href="#">TWITTER</a>
                         </li>
                     </ul>
                 </nav>
-                <div class="footer-black-container">
+                <div class="flex justify-center align-middle">
                     EXPLORE MAIS
                     <img src={arrow} alt="" />
                 </div>
